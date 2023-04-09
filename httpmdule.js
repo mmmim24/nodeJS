e2eb69db0955bafd03675db106aad969bbaeba60
Mustaq Mujahid Mim <mustaqmujahidmim@gmail.com>
2023-04-09 11:43:39 +0600

    heading change

--- httpmdule.js
+++ httpmdule.js
@@ -8,7 +8,7 @@ const server = http.createServer((req,res)=>{
         res.end('Here is our short history  ');
     }
     // res.write('Welcome to home');
-    else res.end(`<h2>It seems that you are trying to access a page that doesn't exist</h1>
+    else res.end(`<h2>It seems that you are trying to access a page that doesn't exist</h2>
     <h3><a href='/'>Back to Home</a></h3>
     `);
 })
